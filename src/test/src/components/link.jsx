45fedb10efcc98aa49558017999fa37b770e478f
dnishi <youyu335@gimal.com>
2022-08-08 23:17:37 +0900

    create

---
+++ src/test/src/components/link.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const Link = () => {
+  const [data, setState] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `${process.env.PUBLIC_URL}/OccurrenceStatusOverseas.json`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+
+      setState(data.itemList);
+    })();
+  }, []);
+  console.log(Array.isArray(data));
+  // if (!Array.isArray(data)) {
+  //   return;
+  // }
+
+  // const list = data.map((item) => {
+  //   return <div>{item.dataName}</div>;
+  // });
+  return <div>213</div>;
+};
+
+export default Link;
